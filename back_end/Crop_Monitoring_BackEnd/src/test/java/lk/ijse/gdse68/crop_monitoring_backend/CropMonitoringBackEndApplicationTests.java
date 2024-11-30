f6b367cd49048e18eec8907d68323cd12835c00d
weerapperuma <weerapperumasahiru@gmail.com>
2024-11-30 09:57:01 +0530

    Initial commit: Added front-end and back-end folders

---
+++ back_end/Crop_Monitoring_BackEnd/src/test/java/lk/ijse/gdse68/crop_monitoring_backend/CropMonitoringBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse68.crop_monitoring_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropMonitoringBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
